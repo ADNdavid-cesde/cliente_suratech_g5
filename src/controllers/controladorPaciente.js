45bb8fffc435075dab3b2aa3991f6f2591b96419
Miguel Velez <miguelvelezpiedrahita@gmail.com>
2024-10-23 16:23:10 -0500

    se crean controladores

---
+++ src/controllers/controladorPaciente.js
@@ -0,0 +1,13 @@
+//Objetivo: Capturar los datos de un formulario
+
+//1. por cada input, select, texarea del formulario se crea una avariable;
+//2. Por cada variable debo utilizar el DOM para asociar el html con js;
+let nombrePaciente = document.getElementById("nombrepaciente");
+let nacimietoPaciente = document.getElementById("nacimientopaciente");
+let ciudadPaciente = document.getElementById("ciudadpaciente");
+let correoPaciente = document.getElementById("correopaciente");
+let telefonoPaciente = document.getElementById("telefonopaciente");
+let ipsPaciente = document.getElementById("ipspaciente");
+let grupoIngresosPaciente = document.getElementById("grupoingresospaciente");
+let afiliacionPaciente = document.getElementById("afiiacionpaciente");
+let polizaPaciente = document.getElementById("polizapaciente");
