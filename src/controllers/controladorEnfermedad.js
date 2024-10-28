df658e032acd5ed4c19b69d1b8334a551aebce9c
ADNdavid <adn6b@hotmail.com>
2024-10-27 19:19:06 -0500

    Se crean controladores Enfermedad, Medicamaneto, Medico y signoVital

---
+++ src/controllers/controladorEnfermedad.js
@@ -0,0 +1,5 @@
+let nombreEnfermedad = document.getElementById("nombreenfermedad");
+let sintomas = document.getElementById("sintomasenfermedad");
+let clasificacion = document.getElementById("casificacionenfermedad");
+let grado = document.getElementById("gradoenfermedad");
+let probVida = document.getElementById("probabilidadvidaenfermedad");
---
+++ src/controllers/controladorSignoVital.js
@@ -0,0 +1,3 @@
+let fechaToma = document.getElementById("fechatoma");
+let descripcion = document.getElementById("descripcion");
+let signoVital = document.getElementById("signovital");
