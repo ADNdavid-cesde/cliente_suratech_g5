fce8fd1f9e3e5c0f05322b03872fb7682ec71f3d
Miguel Velez <miguelvelezpiedrahita@gmail.com>
2024-10-30 15:43:36 -0500

    Se crea vista dashboard

--- src/controllers/controladorPaciente.js
+++ src/controllers/controladorPaciente.js
@@ -2,6 +2,7 @@
 
 //1. por cada input, select, texarea del formulario se crea una avariable;
 //2. Por cada variable debo utilizar el DOM para asociar el html con js;
+/*
 let nombrePaciente = document.getElementById("nombrepaciente");
 let nacimietoPaciente = document.getElementById("nacimientopaciente");
 let ciudadPaciente = document.getElementById("ciudadpaciente");
@@ -45,3 +46,82 @@ botonRegistrarPaciente.addEventListener("click", (evento)=>{
         icon: "success"
       });
 });
+
+*/
+//Objetivo: recibir datos de back y renderizarlos en el Front
+
+//1. quemar o simular los datos
+let pacientes = [
+    {
+        id:1,
+        nombre: "Homero Simpson",
+        anioNacimiento: "1996-01-18",
+        ciudad: "Medellín",
+        correo: "homerJSimpson@gmail.com",
+        telefono: "3014562318",
+        ips: "coopsana centro",
+        poliza: true,
+        grupoIngresos: "C",
+        fechaAfiliacion: "2021-11-23"  
+    },
+    {
+        id: 2,
+        nombre: "Marge Simpson",
+        anioNacimiento: "1996-02-20",
+        ciudad: "Medellín",
+        correo: "margeSimpson@gmail.com",
+        telefono: "3014569874",
+        ips: "saludmed centro",
+        poliza: true,
+        grupoIngresos: "C",
+        fechaAfiliacion: "2021-12-01"
+    },
+    {
+        id: 3,
+        nombre: "Bart Simpson",
+        anioNacimiento: "1999-04-01",
+        ciudad: "Medellín",
+        correo: "bartSimpson@gmail.com",
+        telefono: "3014562345",
+        ips: "coopsana sur",
+        poliza: false,
+        grupoIngresos: "D",
+        fechaAfiliacion: "2022-01-15"
+    },
+    {
+        id: 4,
+        nombre: "Lisa Simpson",
+        anioNacimiento: "2002-05-09",
+        ciudad: "Medellín",
+        correo: "lisaSimpson@gmail.com",
+        telefono: "3014567890",
+        ips: "saludmed norte",
+        poliza: true,
+        grupoIngresos: "B",
+        fechaAfiliacion: "2022-03-10"
+    }
+];
+
+//2. crear una referencia a una etiqueta html donde vamos a renderizar
+let fila = document.getElementById("fila");
+
+//3. se recorren los datos del arreglo para obtenerlos de forma separada
+pacientes.forEach( (paciente) => {
+    console.log(paciente);
+    //4. Se crean columnas
+    let columna = document.createElement("div");
+    columna.classList.add("col");
+
+    //5. Se crea tarjetas
+    let tarjeta = document.createElement("div");
+    tarjeta.classList.add("card","p-5", "h-100", "shadow");
+
+    //6. Se crean etiqueta para mostrar el nombre del paciente
+    let nombre = document.createElement("h2");
+    nombre.textContent = paciente.nombre;
+
+    //7. Ordenando las tarjetas
+    tarjeta.appendChild(nombre);
+    columna.appendChild(tarjeta);
+    fila.appendChild(columna);
+})
